07b241109789e1aefee2e621808eaaee961b70a8
yanagiha <45546914+yanagiha@users.noreply.github.com>
2020-02-03 01:02:03 +0900

    Update utakai.js

--- src/pages/utakai.js
+++ src/pages/utakai.js
@@ -8,7 +8,7 @@ const UtakaiPage = () => (
   <Layout>
     <SEO title="utakai" />
     <h1>Twi歌会(仮)</h1>
-    <p>yanagihaがTwitter上で人を募って開催している短歌の歌会です</p>
+    <p>yanagihaがTwitter上で人を募ってオンラインで開催している短歌の歌会です</p>
     <p>不定期開催でしたが今後は３ヶ月に１回の頻度で定期的に開催していきたいと思います</p>
     <Link to="/">Go back to the home</Link>
   </Layout>
